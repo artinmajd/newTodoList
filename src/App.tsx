42a6567460cc2cf09471d1d6875aa061e10b3fef
Artin Majd <artin.majd@gmail.com>
2021-01-03 20:21:58 +0330

    tasks and checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  const [listItems, setListItems]=useState(
+  [
+  {name: 'task1', done: true},
+  {name: 'task2', done: true},
+  {name: 'task3', done: false},
+  {name: 'task4', done: false},
+  {name: 'task5', done: true},
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul> 
+       {
+         listItems.map((item,index)=>{
+           return(
+           <li> 
+             <input type='checkbox' checked={item.done} 
+             onClick={()=>{
+               setListItems(
+                 listItems.map((innerItem,innerIndex)=>innerIndex!==index? innerItem : {name: innerItem.name, done: !innerItem.done}),
+                 )
+             }}/>
+             {item.name} {item.done? '    Done': '    Undone'}
+           </li>
+           )
+         })
+       }
+      </ul>
     </div>
   );
 }
